63eaf150363eedcdf8626f2e65552ac33c655360
Mert Can Akmızrak <mertcanakmizrak@gmail.com>
2023-10-08 18:28:00 +0300

    c

--- script.js
+++ script.js
@@ -1,37 +1,38 @@
-const video = document.getElementById('video')
-video.addEventListener('touchstart', function(event) {
-  event.preventDefault();
-});
+const video = document.getElementById('video');
+
 Promise.all([
   faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
   faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
   faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
   faceapi.nets.faceExpressionNet.loadFromUri('/models')
-]).then(startVideo)
+]).then(startVideo);
 
 function startVideo() {
-  navigator.getUserMedia(
-    { video: {} },
-    stream => video.srcObject = stream,
-    err => console.error(err)
-  )
+  navigator.mediaDevices.getUserMedia({ video: {} })
+    .then(stream => {
+      video.srcObject = stream;
+      video.play();
+      video.addEventListener('loadedmetadata', () => {
+        // Video yüklendiğinde bu işlemi gerçekleştirin
+        const canvas = faceapi.createCanvasFromMedia(video);
+        document.body.append(canvas);
+        const displaySize = { width: video.width, height: video.height };
+        faceapi.matchDimensions(canvas, displaySize);
+        setInterval(async () => {
+          const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
+          const resizedDetections = faceapi.resizeResults(detections, displaySize);
+          canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+          faceapi.draw.drawDetections(canvas, resizedDetections);
+          faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+          faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+        }, 100);
+      });
+    })
+    .catch(error => {
+      console.error('Kamera erişimi hatası: ', error);
+    });
 }
 
-video.addEventListener('play', () => {
-  const canvas = faceapi.createCanvasFromMedia(video)
-  document.body.append(canvas)
-  const displaySize = { width: video.width, height: video.height }
-  faceapi.matchDimensions(canvas, displaySize)
-  setInterval(async () => {
-    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
-    const resizedDetections = faceapi.resizeResults(detections, displaySize)
-    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-    faceapi.draw.drawDetections(canvas, resizedDetections)
-    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-    faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-  }, 100)
-})
-;
 const startButton = document.getElementById('startButton');
 
 startButton.addEventListener('click', async () => {
@@ -40,17 +41,17 @@ startButton.addEventListener('click', async () => {
     video.srcObject = stream;
     // Kameraya izin verildiğinde videoyu başlat
     video.play();
-} catch (error) {
+  } catch (error) {
     console.error('Kamera erişimi hatası: ', error);
-}
+  }
 });
 
 let isFullscreen = false;
 
 video.addEventListener('click', function() {
-    if (isFullscreen) {
-        video.style.width = 'auto';
-        video.style.height = 'auto';
-    }
-    isFullscreen = !isFullscreen;
+  if (isFullscreen) {
+    video.style.width = 'auto';
+    video.style.height = 'auto';
+  }
+  isFullscreen = !isFullscreen;
 });
